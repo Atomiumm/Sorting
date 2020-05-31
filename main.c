e4f25ca1fc6fb3d27793e57da5374589767bade6
Atomiumm <51803097+Atomiumm@users.noreply.github.com>
2020-05-31 18:40:00 +0200

    publish

---
+++ main.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "sorting.h"
+
+#define ATTEMPTS 10
+#define MAX_TIME 0.001
+#define PUNITION 1000000
+
+
+int augment_count(double total_t, int count){
+	int estimate = count * 1.05;
+	//if(total_t < 0.001) {estimate = count * 2;}
+	//else {estimate = count + (MAX_TIME / total_t * count - count) / 4;}
+	if(estimate < count + 1) estimate = count + 1;
+	return estimate;
+}
+
+
+
+
+int main(int argc, char** argv){
+	clock_t start_t, end_t;
+	double total_t = 0;
+	int* list;
+
+
+	int count = 2;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			int m = StalinSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, m)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, StalinSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, StalinSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			InsertionSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, InsertionSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, InsertionSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			SelectionSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, SelectionSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, SelectionSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			BubbleSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, BubbleSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, BubbleSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			CocktailShakerSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, CocktailShakerSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, CocktailShakerSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			PancakeSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, PancakeSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, PancakeSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			BeadSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, BeadSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, BeadSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			StoogeSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, StoogeSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, StoogeSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			BogoSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		end_t = clock();
+		total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+		//printf("%d, %lf, BogoSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, BogoSort\n", count, total_t);
+
+
+	count = 2;
+	total_t = 0;
+	while(total_t < MAX_TIME){
+		total_t = 0;
+		for(int i = 0; i < ATTEMPTS; i++){
+			list = build_random_array(count);
+			while(check_if_sorted(list, count)) list = build_random_array(count);
+			start_t = clock();
+			MiracleSort(list, count);
+			end_t = clock();
+			if(!check_if_sorted(list, count)) total_t += PUNITION;
+			else total_t += (double)(end_t - start_t) / CLOCKS_PER_SEC / ATTEMPTS;
+			free(list);
+		}
+		//printf("%d, %lf, MiracleSort\n", count, total_t);
+		if(total_t > MAX_TIME) break;
+		count = augment_count(total_t, count);
+	}
+	printf("%d, %lf, MiracleSort\n", count, total_t);
+
+
+	
+
+	return 0;
+}
---
+++ sorting.h
@@ -0,0 +1,209 @@
+#define MAX_TIME 0.001
+
+int* build_random_array(size_t n){
+	int *list = malloc(sizeof(int) * n);
+	time_t t;
+	srand((unsigned) time(&t));
+	for(int* elem = list; elem < list + n; elem++){
+		*elem = rand() % n;
+	}
+	return list;
+}
+
+int check_if_sorted(int *list, size_t n){
+	for(int* elem = list; elem < list + n - 1; elem++){
+		if(*elem > *(elem+1)){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void print_list(int *list, size_t n){
+	printf("\n");
+	for(int* elem = list; elem < list + n; elem++){
+		printf("%d,",*elem);
+	}
+	printf("\n");
+}
+
+
+
+
+int StalinSort(int *list, size_t n){ /*O(n)*/
+	/*Iterate trough the list and ignore each element that is smaller than the previous one*/
+	size_t m = 1;
+	for(int i = 0; i < n; i++){
+		if(list[i] > list[m-1]){
+			list[m] = list[i];
+			m++;
+		}
+	}
+	return m;
+}
+
+void InsertionSort(int *list, size_t n){ /*O(n^2)*/
+	for(int i = 1; i < n; i++){
+		int t = list[i];
+		int j = i-1;
+		for(;j>=0 && list[j]>t; j--){
+			list[j+1] = list[j];
+		}
+		list[j+1] = t;
+	}
+}
+
+void SelectionSort(int *list, size_t n){ /*O(n^2)*/
+	int min = 0;
+	for(int i = 0; i < n; i++){
+		if(list[i] < list[min]){
+			min = i;
+		}
+	}
+	int t = list[0];
+	list[0] = list[min];
+	list[min] = t;
+	if(n>0){
+		SelectionSort(list+1, n-1);
+	}
+}
+
+void BubbleSort(int *list, size_t n){ /*O(n^2)*/
+	for(int i = 0; i < n-1; i++){
+		for(int j = 0; j < n-1; j++){
+			if(list[j] > list[j+1]){
+				int t = list[j];
+				list[j] = list[j+1];
+				list[j+1] = t;
+			}
+		}
+	}
+}
+
+void CocktailShakerSort(int *list, size_t n){ /*O(n^2)*/
+	for(int i = 0; i < n/2; i++){
+		for(int j = i; j < n-i-2; j++){
+			if(list[j] > list[j+1]){
+				int t = list[j];
+				list[j] = list[j+1];
+				list[j+1] = t;
+			}
+		}
+		for(int j = n-i-1; j > i; j--){
+			if(list[j] < list[j-1]){
+				int t = list[j];
+				list[j] = list[j-1];
+				list[j-1] = t;
+			}
+		}
+	}
+}
+
+void PancakeSort(int *list, size_t n){ /*O(n^2)*/
+	/*flipping pancakes*/
+	for(int i = 0; i < n; i++){
+		int min = i;
+		for(int j = i; j < n; j++){
+			if(list[j] < list[min]) min = j;
+		}
+		if(min == i) continue;
+		for(int j = min; j <= min+(n-1-min)/2; j++){
+			int t = list[j];
+			list[j] = list[n-1-j+min];
+			list[n-1-j+min] = t;
+		}
+		for(int j = i; j <= i+(n-1-i)/2; j++){
+			int t = list[j];
+			list[j] = list[n-1-j+i];
+			list[n-1-j+i] = t;
+		}
+	}
+}
+
+void BeadSort(int *list, size_t n){ /*O(sum(list))*/
+	/*Counts the amount of each element in the list then recreates the list*/
+	int max = 0;
+	for(int *elem = list; elem < list + n; elem++){
+		if(*elem > max) max = *elem;
+	}
+	int *weights = calloc(max+1, sizeof(int));
+	for(int i = 0; i < n; i++){
+		weights[list[i]]++;
+	}
+	int m = 0;
+	for(int i = 0; i < max + 1; i++){
+		for(int j = 0; j < weights[i]; j++){
+			list[m+j] = i;
+		}
+		m += weights[i];
+	}
+	free(weights);
+}
+
+void StoogeSort(int *list, size_t n){ /*O(n^2.71)*/
+	if(list[0] > list[n-1]){
+		int t = list[0];
+		list[0] = list[n-1];
+		list[n-1] = t;
+	}
+	if(n > 2){
+		StoogeSort(list, 2*n/3);
+		StoogeSort(list + n/3, n - n/3);
+		StoogeSort(list, 2*n/3);
+	}
+}
+
+/* NOT WORKING void SlowSort(int *list, size_t n){ //O(n^log2(n)/2)
+	if(n > 1){
+		SlowSort(list, n/2);
+		SlowSort(list+n/2, n - n/2);
+		if(list[n-1] < list[n/2]){
+			int t = list[n-1];
+			list[n-1] = list[n/2];
+			list[n/2] = t;
+		}
+		SlowSort(list, n-1);
+	}
+}*/
+
+void BogoSort(int *list, size_t n){ /*O(n*n!)*/
+	/*Shuffle the list until the list is sorted*/
+	clock_t start_t = clock();
+	int count = 0;
+	srand((unsigned) time(NULL));
+	while(!check_if_sorted(list, n)){
+		count++;
+		for(size_t i = 0; i < n - 1; i++){
+			size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+			int t = list[j];
+			list[j] = list[i];
+			list[i] = t;
+		}
+		if((double)(clock() - start_t) / CLOCKS_PER_SEC > MAX_TIME) return;
+	}
+}
+
+void MiracleSort(int *list, size_t n){ /*O(inf)*/
+	/*Wait until the list is miraculously sorted by the hand of god*/
+	clock_t start_t = clock();
+	while((double)(clock() - start_t) / CLOCKS_PER_SEC < MAX_TIME){
+		if(check_if_sorted(list, n)){
+			return;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
